1de202ba69b3c9809c29355cca96ed3f8290dd89
Matthew Duff <matthewlduff@gmail.com>
2014-01-04 13:57:04 -0700

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Matthew Duff on 1/4/14.
+//  Copyright (c) 2014 Matthew Duff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
